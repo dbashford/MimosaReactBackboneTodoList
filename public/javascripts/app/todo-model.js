70a4970b9c2e6fe6e60649bddcfca44fba9bfd0b
david.bashford <dbashford@bericotechnologies.com>
2014-03-13 21:45:59 -0400

    cleaining

--- public/javascripts/app/todo-model.js
+++
@@ -1,4 +0,0 @@
-define(["backbone"],function(t){var e=t.Model.extend({defaults:{title:"",completed:!1},toggle:function(){this.save({completed:!this.get("completed")})}});return e});
-/*
-//@ sourceMappingURL=todo-model.js.map
-*/
--- public/javascripts/main.js
+++
@@ -1,4 +0,0 @@
-require({urlArgs:"b="+(new Date).getTime(),shim:{jquery:{exports:"$"}},paths:{react:"vendor/react/react",backbone:"vendor/backbone/backbone",underscore:"vendor/lodash/lodash.compat",bbLocalStorage:"vendor/backbone.localStorage/backbone.localStorage",jquery:"vendor/zepto/zepto"}},["app/app","app/todo-list-collection","react"],function(e,o,a){a.renderComponent(e({todos:new o}),document.getElementById("container"))});
-/*
-//@ sourceMappingURL=main.js.map
-*/
